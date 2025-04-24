8c6c7d0f69b6c2f163a8d7ae848800eee0171ed5
Mosmosbibi <chinnakrit.phue@bumail.net>
2025-04-24 19:49:07 +0700

    final

--- Assets/Scripts/Camerafollow2d.cs
+++ Assets/Scripts/Camerafollow2d.cs
@@ -1,12 +1,12 @@
 using System;
 using UnityEngine;
 
-public class Camerafollow2d : MonoBehaviour
+public class CameraFollow2d : MonoBehaviour
 {
    private Vector3 offset = new Vector3(0f, 0f, -10f); 
    private float smoothTime = 0.25f;
    private Vector3 velocity = Vector3.zero;
-   
+  
    [SerializeField] private Transform target;
 
    private void Update()
